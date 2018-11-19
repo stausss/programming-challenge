9a1bc4c708107fe2a0104d762d6b4b8b1edf270d
stausss <stefanstauss@yahoo.de>
2018-11-19 23:38:14 +0100

    feat(reader): add reader scaffold

---
+++ src/main/java/de/exxcellent/challenge/reader/CSVReader.java
@@ -0,0 +1,16 @@
+package de.exxcellent.challenge.reader;
+
+import de.exxcellent.challenge.data.Processable;
+
+import java.util.ArrayList;
+
+public class CSVReader implements FileReader  {
+
+    public CSVReader() {
+    }
+
+    @Override
+    public ArrayList<Processable> read(String fileName, String idLabel, String arg1Label, String arg2Label) {
+        return null;
+    }
+}
---
+++ src/main/java/de/exxcellent/challenge/reader/FileReader.java
@@ -0,0 +1,9 @@
+package de.exxcellent.challenge.reader;
+
+import de.exxcellent.challenge.data.Processable;
+
+import java.util.ArrayList;
+
+public interface FileReader {
+    ArrayList<Processable> read(String fileName, String idLabel, String arg1Label, String arg2Label);
+}
