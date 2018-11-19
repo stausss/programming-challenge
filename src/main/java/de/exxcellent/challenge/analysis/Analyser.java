7539093938a52ce873d6f5ac6047b369226222e6
stausss <stefanstauss@yahoo.de>
2018-11-19 22:49:49 +0100

    feat(analysis): add analysis scaffold

---
+++ src/main/java/de/exxcellent/challenge/analysis/Analyser.java
@@ -0,0 +1,9 @@
+package de.exxcellent.challenge.analysis;
+
+import de.exxcellent.challenge.data.Processable;
+
+import java.util.ArrayList;
+
+public interface Analyser {
+    public String findSmallestDiff(ArrayList<Processable> analyseData);
+}
---
+++ src/main/java/de/exxcellent/challenge/analysis/DataAnalyser.java
@@ -0,0 +1,16 @@
+package de.exxcellent.challenge.analysis;
+
+import de.exxcellent.challenge.data.Processable;
+
+import java.util.ArrayList;
+
+public class DataAnalyser implements Analyser {
+
+    public DataAnalyser() {
+    }
+
+    @Override
+    public String findSmallestDiff(ArrayList<Processable> analyseData) {
+        return null;
+    }
+}
